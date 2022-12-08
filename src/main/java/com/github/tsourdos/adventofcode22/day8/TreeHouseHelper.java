53934b5a089e82670bd58017385a823d2c610939
s.tsourdos <tsourdos@hotmail.com>
2022-12-08 12:50:54 +0200

    2022 day8 solution

--- src/main/java/com/github/tsourdos/adventofcode22/day8/TreeHouseHelper.java
+++ src/main/java/com/github/tsourdos/adventofcode22/day8/TreeHouseHelper.java
@@ -10,7 +10,6 @@ public class TreeHouseHelper {
         String[] rows = input.split("\n");
         int x = rows.length;
         int y = rows[0].length();
-        //  Character[][] forest = new Character[x][y];
         Map<Pos, Integer> forest = new HashMap<>();
         for (int i = 0; i < y; i++) {
             char[] chars = rows[i].toCharArray();
@@ -18,17 +17,13 @@ public class TreeHouseHelper {
                 forest.put(new Pos(j, i), toInt(chars[j]));
             }
         }
-//        System.out.println(forest);
-        boolean[][] visible = new boolean[x][y];
-        int vis=0;
+        int vis = 0;
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
                 if (i == 0 || j == 0 || i == y - 1 || j == x - 1) {
-                    visible[i][j] = true;
                     vis++;
                 } else {
                     Boolean visible1 = isVisible(forest, i, j, x, y);
-                    visible[i][j] = visible1;
                     if (visible1)
                         vis++;
                 }
@@ -41,8 +36,9 @@ public class TreeHouseHelper {
             for (int j = 0; j < x; j++) {
                 if (i == 0 || j == 0 || i == y - 1 || j == x - 1) {
 
-                }else
-                    points.add(getTreePoints(forest, i,j,x,y));
+                } else {
+                    points.add(getTreePoints(forest, i, j, x, y));
+                }
             }
         }
         System.out.println(points.stream().max(Comparator.naturalOrder()));
@@ -52,11 +48,11 @@ public class TreeHouseHelper {
         int height = forest.get(new Pos(i, j));
 
         int points = 0;
-        if (positions.isEmpty()||forest.get(positions.get(0))>=height) {
+        if (positions.isEmpty() || forest.get(positions.get(0)) >= height) {
             return 1;
         }
-        for (Pos pos:positions) {
-            if (forest.get(pos)<height)
+        for (Pos pos : positions) {
+            if (forest.get(pos) < height)
                 points++;
             else
                 return points + 1;
@@ -66,35 +62,36 @@ public class TreeHouseHelper {
 
     private Boolean isVisible(Map<Pos, Integer> forest, int i, int j, List<Pos> positions) {
         int height = forest.get(new Pos(i, j));
-        for (Pos pos:positions) {
-            if (forest.get(pos)>=height)
+        for (Pos pos : positions) {
+            if (forest.get(pos) >= height)
                 return false;
         }
         return true;
     }
+
     private int getTreePoints(Map<Pos, Integer> forest, int i, int j, int x, int y) {
         return
-            getTreePoints(forest, i, j, rev(0,i).boxed().map(ver-> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList()))*
-            getTreePoints(forest, i, j, IntStream.range(i+1,x).boxed().map(ver-> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList()))*
-            getTreePoints(forest, i, j, rev(0,j).boxed().map(hor-> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList()))*
-            getTreePoints(forest, i, j, IntStream.range(j+1,y).boxed().map(hor-> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList()))
-        ;
+                getTreePoints(forest, i, j, rev(0, i).boxed().map(ver -> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) *
+                        getTreePoints(forest, i, j, IntStream.range(i + 1, x).boxed().map(ver -> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) *
+                        getTreePoints(forest, i, j, rev(0, j).boxed().map(hor -> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList())) *
+                        getTreePoints(forest, i, j, IntStream.range(j + 1, y).boxed().map(hor -> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList()))
+                ;
     }
 
     private IntStream rev(int from, int to) {
-        return IntStream.range(from,to).map(i->to-i+from-1);
+        return IntStream.range(from, to).map(i -> to - i + from - 1);
     }
 
     private Boolean isVisible(Map<Pos, Integer> forest, int i, int j, int x, int y) {
         return
-            isVisible(forest,i,j,IntStream.range(0,i).boxed().map(ver-> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) ||
-            isVisible(forest,i,j,IntStream.range(i+1,x).boxed().map(ver-> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) ||
-            isVisible(forest,i,j,IntStream.range(0,j).boxed().map(hor-> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList())) ||
-            isVisible(forest,i,j,IntStream.range(j+1,y).boxed().map(hor-> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList()))
-        ;
+                isVisible(forest, i, j, IntStream.range(0, i).boxed().map(ver -> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) ||
+                        isVisible(forest, i, j, IntStream.range(i + 1, x).boxed().map(ver -> new Pos(ver, j)).filter(forest::containsKey).collect(Collectors.toList())) ||
+                        isVisible(forest, i, j, IntStream.range(0, j).boxed().map(hor -> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList())) ||
+                        isVisible(forest, i, j, IntStream.range(j + 1, y).boxed().map(hor -> new Pos(i, hor)).filter(forest::containsKey).collect(Collectors.toList()))
+                ;
     }
 
-    private int toInt(Character character){
+    private int toInt(Character character) {
         return Character.getNumericValue(character);
     }
 
