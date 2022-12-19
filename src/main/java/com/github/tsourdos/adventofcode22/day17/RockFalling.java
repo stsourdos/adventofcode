9399a75e6a374d0c67b17edd55bb617f951c9bb8
s.tsourdos <tsourdos@hotmail.com>
2022-12-19 12:05:25 +0200

    2022 day 17 solution

---
+++ src/main/java/com/github/tsourdos/adventofcode22/day17/RockFalling.java
@@ -0,0 +1,23 @@
+package com.github.tsourdos.adventofcode22.day17;
+
+public class RockFalling {
+    private String in;
+
+    public void analyse(String input) {
+        in = input;
+        int inIndex = 0;
+        char[][] array = new char[5000][7];
+        for (int i = 0; i < 2021; i++) {
+            boolean bottomHit = false;
+            while (!bottomHit){
+
+            }
+            if (inIndex >= input.length()) {
+                inIndex = 0;
+            }
+            char c = input.charAt(inIndex);
+
+            inIndex++;
+        }
+    }
+}
