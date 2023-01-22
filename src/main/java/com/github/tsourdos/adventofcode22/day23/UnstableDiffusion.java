fce9b38652422123386de3091090afb4c6f7de16
s.tsourdos <tsourdos@hotmail.com>
2023-01-22 23:12:19 +0200

    2022 day23 solution

--- src/main/java/com/github/tsourdos/adventofcode22/day23/UnstableDiffusion.java
+++ src/main/java/com/github/tsourdos/adventofcode22/day23/UnstableDiffusion.java
@@ -9,9 +9,91 @@ import static com.github.tsourdos.adventofcode22.day23.UnstableDiffusion.Dir.R;
 
 public class UnstableDiffusion {
 
+    public static void main(String[] args) {
+        String input = "....#..\n" +
+                "..###.#\n" +
+                "#...#.#\n" +
+                ".#...##\n" +
+                "#.###..\n" +
+                "##.#.##\n" +
+                ".#..#..";
+        input = "...##..#.##.#..####....###..#.#..#.###.######.###...#...#.....###..##.#\n" +
+                "..##.##.######.#.###.#.......#...####..####.###...#....#.##..#.#####.##\n" +
+                "###.#.###.#.#..#.#.#..#.#.#####.#....#..###..#..#......####.#..####..##\n" +
+                ".##...#.#..#.#..#.#.#.#....#.####...#..#..##.#...##.#..#.#.#.#......###\n" +
+                "##..#.#.#.#.#.##.##.#.#...#.###.#...####...##..##..#.###.##.######..#.#\n" +
+                "#.#.#....##.###....#.......######.....##.####.#.###...###.#.#...#####..\n" +
+                ".##.....######.#......###.###.....#####...#..#####.#.#.##....#.#....##.\n" +
+                "#...##.#....#.#.###.##.#..###.#...##...#.##.##.#..#.####.#.#.#.#.#...#.\n" +
+                ".#.#.####.#...#..####.#...##.#.#.###.##..###..#.#.#.####.#.....#.##..#.\n" +
+                ".#..#.#..##..######..#.##.##.##.#....#..#..#.##..###.#.#....###......#.\n" +
+                ".####...#.##..##....#...#####.##.###......#.......#..##.#....###.......\n" +
+                ".....#.#####...####.###..#.#.###.#..#.......#..........##.#####.#...#..\n" +
+                ".....##..####...#.#...#.###...#..##.###......#.#..#....##...####....##.\n" +
+                ".####..#.#.####.##.##..###..#...#.#.##.#..####.....####.#.#.##...#.....\n" +
+                "..#..#..##..#.#....##..#####...#...#####....#....#..###.###.####.#...#.\n" +
+                ".###.#.#####.###...#.#.##.##..###.#.##.##.####.###..#.......##...###...\n" +
+                "####.#.#....#.###.#.##..###.#.##..##.#..##..###.####...###....#####..##\n" +
+                ".##.#####..#.#.#.#...#......###.###.#....####.....#..#......##...###...\n" +
+                "#.##.###.####.##.#..#.#...#..##..##....####.#..#..####...#.##.##.#.#..#\n" +
+                ".....##.#####..#..#.##.......#######..##.##.....#..###...#####.....##..\n" +
+                "..#..###...####.#.#.......###...##..#.######...#....#...#..##...###...#\n" +
+                "##.#...###..#.###.##...#.#..#.#.##..#.###..#..#.####......##.#.#.####..\n" +
+                ".###...###..#.#.##.###.##..##.#.#...###....#..##.#..###..##..####.#..#.\n" +
+                "#..#.#....#...#.###.#....##..#....##.#...#.....#..#......#.#...#.#.###.\n" +
+                ".##.#....##.##..###..#.#..####.###...##.#.##.####..#.####..#..##.##....\n" +
+                "#.#.####..#...#.#.####....#..###..#..##....##...#.#.#.#.##.#...#.###...\n" +
+                "######..#.#####.###..##.#####.#..#.#.#.#.......##.#.#..#....##.#.#...##\n" +
+                "####....##.#..###....###.....#..#...##.##...#......#..##..#.#...####..#\n" +
+                ".##..#####.#..##..##...#.#.#..#....#.####..##.......#.##.#.###.#.#..##.\n" +
+                ".##...####..#..##..##.###....#....#..##.#.#.##......#.#.##...##.#####..\n" +
+                "....#..###.##..##...###.##.##...#....#..####.#####.###..#.###.#.#.#..##\n" +
+                "##.#.#.#.#...#######..#.##..#.#.#.####.#.###...##.######.##.....##.#..#\n" +
+                "...###..#.##.#..#.###.....#####.##.##.#.##.#.#..##..###.#........#.#.##\n" +
+                ".#....#.#.#..####....#.###.#.#.#..##..####..#..#.#...####..#..#..#.##..\n" +
+                "##...#.#.##.##.#.###.#.#...######.###.....####.##..######..#..##.......\n" +
+                "#.##.#.###..#....##....#.##.##...#####.##.######.....##.#...#.##..#.###\n" +
+                "#..#....##.###..####..#..##.###....#..###....########...#..##.######...\n" +
+                "##..####..##.#.###..#..####.##.##......##...###.##.###.###..#.###.#####\n" +
+                "##.#.#..#.#.###.....#.#.#..####.#.##########...##.#.#.###.#.##..#.##.#.\n" +
+                "#.##########.#.#.#..#######....#..##.#.###..##.....#.#..##..###.#.....#\n" +
+                "#.##.##..##.##.########..#.#.###.#..##...#######.######.#.#.###..#..##.\n" +
+                "...#.#..###...#.#..#.#.#.######...####.#..######.######...#.#...#.#.##.\n" +
+                "#######.####.###.....##.#..#####.#....####....##.#.##..##.##..#....#.#.\n" +
+                ".#.#.#..##.#.#....##.###.......##...#..##..####....####.#.#.#.###..##..\n" +
+                ".#..####.#...#..#.##...#####.##..##.###......#.#..#.#.#.#..####.#.##..#\n" +
+                ".###.#.####.####.####..#..#..#.#...#...#####....##.#..#.#####.##..###..\n" +
+                ".#..#.##..#...##.##......#.#.#......##.#..#...####...##.#.##.#.#.####..\n" +
+                "###.#.##.#.#########..##.#..##.#.#..#.#.#...##.##.#.#...#.#####.......#\n" +
+                "..###...#..#..#.##.#.#.#.###....##..#..###.#.#.#........#.#...#..##.#.#\n" +
+                ".###....##.########..##.#.##..#.##...##.###.#...#..#..##..##...##...#.#\n" +
+                "##...#..##.#.#.##.#.#....##....###.##...###.#####..##.....##........#.#\n" +
+                "..#...#.######..#.....#.#.##.#.#.###..##...##.#..#..#.#......#.##..#...\n" +
+                "#...........#..##.####.###.#.....##....##.#.##..#...#..#####...##...#.#\n" +
+                "....#..####...##..###..#####.#..####.###..#.##..###..#.#.##.#.########.\n" +
+                "..#.#......####..#.##..#.###.##.#.###..#..#.###.#....#.##.##..#..##..#.\n" +
+                "####.##.#.###.#.#######.###.......##..####.#.....##...##...###..###..#.\n" +
+                "#....#..####.#..#.#.#.###.##........##.#..##.#..#.#..##..#...##.#...##.\n" +
+                "#...###.###.#.#..##......#..###..##.#....###...#...##..#..#####.####.#.\n" +
+                "######..####...####.##.....###..#...###.##......#..#.##.##.###.##.####.\n" +
+                "..##......#......##.####..#..###....#..#..#.....#..##.....###.####..#.#\n" +
+                "#####.###..##.....#....#...##.#.##...#.#.#.#..##.#..#...#..####...##...\n" +
+                ".##..#.#...##..##..#.###.#.#.#...#####..#.####..###..##....##.####..#..\n" +
+                "##...#####....#.#.##.###.#.#..##.#...#.#.#.####.....#.#..##.#...#......\n" +
+                "#......###.###.###......##.####.#.####...##..#.#.#......######.#.#.#..#\n" +
+                "..##.###.##.#.#.###.#.###.####..#..###########.##.##.#..#......##.#.###\n" +
+                "..#.###....##.....###......##.##...##.#..###..#..#.#..###....######.##.\n" +
+                "#####.#####.#.#.####...#########....#.....#...##..#.....###..###.######\n" +
+                ".##.#####.#....#.#.#......##..##.#####...#.####.#.##..#..##..####...#..\n" +
+                "####.##.###.#..#...##...##.#..##.....#..##..#####...........##..#..###.\n" +
+                "##.##.####.#.#.....#.#.#..#..###..###.........#..#.#..####.##........##\n" +
+                ".#.###......#...#..##.##...#..#..#.#..#.##.#.#.####..###.....#.###.#..#";
+        new UnstableDiffusion().analyse(input);
+    }
+
     public void analyse(String input) {
         String[] split = input.split("\n");
-        Map<Integer,Pos> init = new HashMap();
+        Map<Integer, Pos> init = new HashMap<>();
         int elf = 0;
         for (int i = 0; i < split.length; i++) {
             String line = split[i];
@@ -23,10 +105,35 @@ public class UnstableDiffusion {
                 }
             }
         }
-        for (int round = 1; round <= 10; round++) {
+        for (int round = 1; round <= Integer.MAX_VALUE; round++) {
             List<Dir> dirs = getDirections(round);
-            List<Pos> proposed = round1(init, dirs);
+            Map<Integer, Pos> proposed = round1(init, dirs);
+            if (proposed.isEmpty()) {
+                System.out.println("Part-2: " + round);
+                break;
+            }
+            for (Map.Entry<Integer, Pos> e : init.entrySet()) {
+                if (proposed.containsKey(e.getKey()))
+                    init.put(e.getKey(), proposed.get(e.getKey()));
+            }
         }
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (Pos pos : init.values()) {
+            if (pos.x > maxX)
+                maxX = pos.x;
+            if (pos.x < minX)
+                minX = pos.x;
+            if (pos.y > maxY)
+                maxY = pos.y;
+            if (pos.y < minY)
+                minY = pos.y;
+        }
+        int distX = maxX - minX + 1;
+        int distY = maxY - minY + 1;
+        System.out.println("Part-1: " + ((distY * distX) - init.size()));
     }
 
     private List<Dir> getDirections(int round) {
@@ -39,14 +146,20 @@ public class UnstableDiffusion {
         return Arrays.asList(R, U, D, L);
     }
 
-    private List<Pos> round1(Map<Integer, Pos> init, List<Dir> dirs) {
-        List<Pos> proposed = new ArrayList<>();
-        for (Map.Entry<Integer,Pos> pos : init.entrySet()) {
+    private Map<Integer, Pos> round1(Map<Integer, Pos> init, List<Dir> dirs) {
+        Map<Integer, Pos> proposed = new HashMap<>();
+        for (Map.Entry<Integer, Pos> pos : init.entrySet()) {
             if (noNeighbor(init.values(), pos.getValue()))
                 continue;
             for (Dir dir : dirs) {
                 if (!hasNeighbor(init.values(), pos.getValue(), dir)) {
-                    // move
+                    Pos newPos = new Pos(pos.getValue()).move(dir);
+                    if (proposed.containsValue(newPos)) {
+                        Integer key = getKeyByValue(proposed, newPos);
+                        proposed.remove(key);
+                    } else
+                        proposed.put(pos.getKey(), newPos);
+                    break;
                 }
             }
         }
@@ -54,22 +167,26 @@ public class UnstableDiffusion {
     }
 
     private boolean noNeighbor(Collection<Pos> init, Pos pos) {
-        return !init.contains(new Pos(pos.x, pos.y - 1)) && !init.contains(new Pos(pos.x - 1, pos.y - 1)) && !init.contains(new Pos(pos.x + 1, pos.y - 1))
-                && !init.contains(new Pos(pos.x, pos.y + 1)) && !init.contains(new Pos(pos.x - 1, pos.y + 1)) && !init.contains(new Pos(pos.x + 1, pos.y + 1))
-                && !init.contains(new Pos(pos.x - 1, pos.y)) && !init.contains(new Pos(pos.x - 1, pos.y - 1)) && !init.contains(new Pos(pos.x - 1, pos.y + 1))
-                && !init.contains(new Pos(pos.x + 1, pos.y)) && !init.contains(new Pos(pos.x + 1, pos.y - 1)) && !init.contains(new Pos(pos.x + 1, pos.y + 1));
+        return !init.contains(new Pos(pos.x, pos.y - 1)) &&
+                !init.contains(new Pos(pos.x - 1, pos.y - 1)) &&
+                !init.contains(new Pos(pos.x + 1, pos.y - 1)) &&
+                !init.contains(new Pos(pos.x, pos.y + 1)) &&
+                !init.contains(new Pos(pos.x - 1, pos.y + 1)) &&
+                !init.contains(new Pos(pos.x + 1, pos.y + 1)) &&
+                !init.contains(new Pos(pos.x - 1, pos.y)) &&
+                !init.contains(new Pos(pos.x + 1, pos.y));
 
     }
 
     private boolean hasNeighbor(Collection<Pos> init, Pos pos, Dir dir) {
         if (dir.equals(U))
-            return init.contains(new Pos(pos.x, pos.y - 1)) && init.contains(new Pos(pos.x - 1, pos.y - 1)) && init.contains(new Pos(pos.x + 1, pos.y - 1));
+            return init.contains(new Pos(pos.x, pos.y - 1)) || init.contains(new Pos(pos.x - 1, pos.y - 1)) || init.contains(new Pos(pos.x + 1, pos.y - 1));
         if (dir.equals(D))
-            return init.contains(new Pos(pos.x, pos.y + 1)) && init.contains(new Pos(pos.x - 1, pos.y + 1)) && init.contains(new Pos(pos.x + 1, pos.y + 1));
+            return init.contains(new Pos(pos.x, pos.y + 1)) || init.contains(new Pos(pos.x - 1, pos.y + 1)) || init.contains(new Pos(pos.x + 1, pos.y + 1));
         if (dir.equals(L))
-            return init.contains(new Pos(pos.x - 1, pos.y)) && init.contains(new Pos(pos.x - 1, pos.y - 1)) && init.contains(new Pos(pos.x - 1, pos.y + 1));
+            return init.contains(new Pos(pos.x - 1, pos.y)) || init.contains(new Pos(pos.x - 1, pos.y - 1)) || init.contains(new Pos(pos.x - 1, pos.y + 1));
         if (dir.equals(R))
-            return init.contains(new Pos(pos.x + 1, pos.y)) && init.contains(new Pos(pos.x + 1, pos.y - 1)) && init.contains(new Pos(pos.x + 1, pos.y + 1));
+            return init.contains(new Pos(pos.x + 1, pos.y)) || init.contains(new Pos(pos.x + 1, pos.y - 1)) || init.contains(new Pos(pos.x + 1, pos.y + 1));
         throw new IllegalArgumentException("Invalid dir " + dir);
     }
 
@@ -80,9 +197,34 @@ public class UnstableDiffusion {
     public static class Pos {
         int x, y;
 
+        public Pos(Pos pos) {
+            this.x = pos.x;
+            this.y = pos.y;
+        }
+
         public Pos(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        public Pos up() {
+            y--;
+            return this;
+        }
+
+        public Pos down() {
+            y++;
+            return this;
+        }
+
+        public Pos left() {
+            x--;
+            return this;
+        }
+
+        public Pos right() {
+            x++;
+            return this;
         }
 
         @Override
@@ -105,5 +247,26 @@ public class UnstableDiffusion {
                     ", y=" + y +
                     '}';
         }
+
+        public Pos move(Dir dir) {
+            if (dir == U)
+                return up();
+            if (dir == D)
+                return down();
+            if (dir == L)
+                return left();
+            if (dir == R)
+                return right();
+            throw new IllegalArgumentException("Bad arg");
+        }
+    }
+
+    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
